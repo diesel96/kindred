e8f133780370d5a1ae129341a8e4e1863a07dabd
Jericho FIllon <jerichofilloncoding@gmail.com>
2020-11-10 17:31:37 -0800

    Styling additions and revisions

--- scripts/core.js
+++ scripts/core.js
@@ -3,7 +3,6 @@ const navDOM = document.querySelector(".nav-bar");
 const navMenu = document.querySelector(".nav-menu");
 const navSearch = document.querySelector(".nav-search");
 const navShop = document.querySelector(".nav-shop");
-const navLook = document.querySelector(".lookbook");
 
 let smallDevice = window.matchMedia("(min-width: 374px)");
 let mediumDevice = window.matchMedia("(max-width: 677px)");
@@ -13,10 +12,10 @@ function handleDeviceChange(event) {
   if (event.matches) {
     navMenu.classList.remove("hidden");
     navShop.classList.add("hidden");
-    navLook.classList.add("hidden");
     navSearch.classList.add("hidden");
   }
 }
 
 mediumDevice.addEventListener("change", handleDeviceChange);
-mediumDevice.addEventListener('DOMContentLoaded', handleDeviceChange);
+mediumDevice.addEventListener("click", handleDeviceChange);
+mediumDevice.addEventListener("DOMContentLoaded", handleDeviceChange);
